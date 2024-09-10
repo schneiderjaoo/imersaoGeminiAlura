f6a84027243c7d88711e6612f5f762eaaa6df56f
João Schneider <joaoguischneiders@gmail.com>
2024-09-09 21:44:21 -0300

    Melhorias

---
+++ dados.js
@@ -0,0 +1,28 @@
+let tiposDeDados = [
+    {
+        nome: "String",
+        descricao: "Sequência de caracteres. Utilizada para armazenar texto.",
+        link: "Olá, mundo!",
+    },
+    {
+        nome: "Number",
+        descricao: "Número de ponto flutuante de dupla precisão. Inclui inteiros e decimais.",
+        exemplo: 42,
+        link: ["inteiro", "decimal"]
+    },
+    {
+        nome: "Boolean",
+        descricao: "Valor lógico, pode ser verdadeiro (true) ou falso (false).",
+        link: true
+    },
+    {
+        nome: "null",
+        descricao: "Valor que representa a ausência de qualquer valor objeto.",
+        link: null
+    },
+    {
+        nome: "undefined",
+        descricao: "Valor que indica que uma variável foi declarada, mas ainda não foi atribuída.",
+        link: undefined
+    }
+];
--- script.js
+++ script.js
@@ -1,1 +1,44 @@
-console.log('Hello, world!');
+//console.log(tiposDeDados[0]);
+function pesquisar(){
+    let section = document.getElementById("resultadosDaPesquisa")
+
+    let resultados = "";
+    
+    //para cada dado dentro da lista de dados
+    for(let dado of tiposDeDados){
+        resultados += ` 
+            <div class="item-resulado">
+                <h2>
+                    <a href="${dado.link}" target="_blank">
+                        ${dado.nome}!
+                    </a>
+                </h2>
+                <p class="descricao-meta">
+                    ${dado.descricao}"
+                </p>
+            </div>`;
+        }   
+        
+    section.innerHTML = resultados;
+}
+/*
+let section = document.getElementById("resultadosDaPesquisa")
+
+let resultados = "";
+
+//para cada dado dentro da lista de dados
+for(let dado of tiposDeDados){
+    resultados += ` 
+        <div class="item-resulado">
+            <h2>
+                <a href="${dado.link}" target="_blank">
+                    ${dado.nome}!
+                </a>
+            </h2>
+            <p class="descricao-meta">
+                ${dado.descricao}"
+            </p>
+        </div>`;
+    }   
+    
+section.innerHTML = resultados; */
