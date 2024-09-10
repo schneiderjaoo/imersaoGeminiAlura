53d222451e038afaeafca48e9bae8330dbf267ba
João Schneider <joaoguischneiders@gmail.com>
2024-09-10 13:00:04 -0300

    Refactor input field and search functionality

--- script.js
+++ script.js
@@ -3,42 +3,34 @@ function pesquisar(){
     let section = document.getElementById("resultadosDaPesquisa")
 
     let resultados = "";
+    let nome = "";
+    let descricao = "";
     
+    let campoPesquisa = document.getElementById("campoPesquisa").value;
+    console.log(campoPesquisa);
+    if(campoPesquisa == ""){
+        section.innerHTML = "A barra de pesquisa está vazia!";
+        return;
+    }
+
     //para cada dado dentro da lista de dados
     for(let dado of tiposDeDados){
-        resultados += ` 
-            <div class="item-resulado">
-                <h2>
-                    <a href="${dado.link}" target="_blank">
-                        ${dado.nome}!
-                    </a>
-                </h2>
-                <p class="descricao-meta">
-                    ${dado.descricao}"
-                </p>
-            </div>`;
+        nome = campoPesquisa.toLowerCase();
+        descricao = campoPesquisa.toLowerCase();
+        if(nome.includes(campoPesquisa) || descricao.includes(campoPesquisa)){
+            resultados += ` 
+                <div class="item-resulado">
+                    <h2>
+                        <a href="${dado.link}" target="_blank">
+                            ${dado.nome}!
+                        </a>
+                    </h2>
+                    <p class="descricao-meta">
+                        ${dado.descricao}"
+                    </p>
+                </div>`;
+            }
         }   
         
     section.innerHTML = resultados;
 }
-/*
-let section = document.getElementById("resultadosDaPesquisa")
-
-let resultados = "";
-
-//para cada dado dentro da lista de dados
-for(let dado of tiposDeDados){
-    resultados += ` 
-        <div class="item-resulado">
-            <h2>
-                <a href="${dado.link}" target="_blank">
-                    ${dado.nome}!
-                </a>
-            </h2>
-            <p class="descricao-meta">
-                ${dado.descricao}"
-            </p>
-        </div>`;
-    }   
-    
-section.innerHTML = resultados; */
